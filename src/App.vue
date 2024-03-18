d453feb8299737397309b54985e73314f4f72204
Denis-Front <oriontorion@mail.ru>
2024-03-18 16:17:26 +0500

    init project

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+  import './main.scss'
+  import Header from "@/components/Header/Header.vue";
+</script>
+
+<template class="container">
+  <Header />
+</template>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import './Header.scss'
+</script>
+
+<template>
+  <header class="header">
+    <div class="header__wrapper">
+      <div class="header__logo">
+        <picture class="header__image">
+          <img src="" alt="logo">
+        </picture>
+      </div>
+    </div>
+  </header>
+</template>
