21f3c96eeb05e46ff0fb099eb735b9ae416bdba0
Denis-Front <oriontorion@mail.ru>
2024-03-21 15:47:29 +0500

    fix back banner

--- src/components/Banner/Back/Back.vue
+++ src/components/Banner/Back/Back.vue
@@ -1,6 +1,6 @@
 <script setup>
-  import './Back.scss'
-  import pictures from "./images.js";
+import './Back.scss'
+import pictures from "./images.js";
 </script>
 
 <template>
@@ -9,11 +9,32 @@
         v-for="picture in pictures"
         :src="'/src/assets/images/Banner/'+ picture.img +'.webp'"
         :alt="picture.img"
-        :style="'top:' + picture.top + 'px; left:' + picture.left + 'px'"
+        :style="'top:' + picture.top + 'px; ' +'left:' + picture.left + 'px;' +'transform: ' + transform"
     >
   </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      transform: '',
+    }
+  },
+  mounted() {
+    this.transformChange();
+  },
+  methods: {
+    transformChange() {
+      window.addEventListener('mousemove', (e) => {
+        if (document.documentElement.offsetWidth > 1215) {
+          let x = e.x;
+          let y = e.y;
+          this.transform = `translateX(${(x) / 100}%) translateZ(${0}px) translateY(${(y) / 100}%)`;
+        }
 
+      })
+    }
+  }
+}
 </script>
